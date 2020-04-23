6d6b740779d79a7ae5b2afecd6d59caa2255a385
james fisk <jamesafisk@gmail.com>
2020-04-23 17:03:02 +0100

    Added news tick strings and scroller

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import WeatherServer
 import read_solar
 import logging
 import logging.handlers
+import ScrollText
 from datetime import datetime
 
 os.putenv('SDL_FBDEV', '/dev/fb1')
@@ -25,7 +26,7 @@ boxrect = box.get_rect()
 pg.mouse.set_visible(False)
 
 class MainClass():
-        light = (50, 50, 50)
+        light = (150, 150, 150)
         def init_logger(self):
                 level = logging.DEBUG
                 logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s',level=level)
@@ -53,10 +54,19 @@ class MainClass():
                 showstatus = True
                 
                 startTimeForStatus = time.time()
+                startTimeForNewsTick = time.time()
 
                 pg.font.init()
-                font = pg.font.SysFont("comicsansms", 64)
+                font = pg.font.SysFont("comicsansms", 40)
                 clock = pg.time.Clock()
+
+                newsindex=0
+
+                news = ["The news", "The other news", "Here is the news"] 
+
+                spheres = (
+                        ScrollText.ScrollText(screen, news[newsindex], 100, pg.Color(255,255,0)),
+                )         
 
                 # set the center of the rectangular object. 
                 while running:
@@ -72,23 +82,41 @@ class MainClass():
                                 screen.blit(box, boxrect)
 
                         elapsedTimeForStatus = time.time() - startTimeForStatus
+                        elapsedTimenewsTick = time.time() - startTimeForNewsTick
                         
                         if (elapsedTimeForStatus > 1):
                                 showstatus = not showstatus
                                 startTimeForStatus = time.time()
+
+                        if (elapsedTimenewsTick > 10):
+                                spheres = (
+                                        ScrollText.ScrollText(screen, news[newsindex], 100, pg.Color(255,255,0)),
+                                )
+                                newsindex = newsindex + 1
+                                if (newsindex >= len(news)):
+                                        newsindex=0  
+                                startTimeForNewsTick = time.time()
+
                                 
                         temp = mythread.temp
                         stringtooutput = str(int(temp)) + u'\N{DEGREE SIGN}'
                                 
-                        text = font.render(stringtooutput, True, currentLight, (0,0,0)) 
-                        screen.blit(text,(410,268))
+                        temperature = font.render(stringtooutput, True, currentLight, (0,0,0))
+                        temperatureSize = temperature.get_size()
+                        screen.blit(temperature,(width-temperatureSize[0],height-temperatureSize[1]))
                         
-                        inverterWatt = font.render(str(inverterThread.watt_now), True, currentLight, (0,0,0))
-                        screen.blit(inverterWatt, (480/2, 380/2))
+                        if (inverterThread.watt_now >  1000) :
+                                inverterWatt = font.render(str(inverterThread.watt_now/100)+' Kw', True, currentLight, (0,0,0))
+                        else:
+                                inverterWatt = font.render(str(inverterThread.watt_now)+' w', True, currentLight, (0,0,0))
+
+                        screen.blit(inverterWatt, (240, 150))
                         
                         now = datetime.now()
                         timeoutput = font.render(now.strftime("%H:%M:%S"), True, currentLight, (0,0,0))
                         screen.blit(timeoutput, (315, 5))
+                        for thing in spheres:
+                                thing.update()                        
                         pg.display.flip()
                         clock.tick(25)
                         
